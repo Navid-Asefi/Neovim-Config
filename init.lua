78947ae0a163df3df407419f1bdf60b7e8d0f69c
Navid <navidasefi@yahoo.com>
2025-03-26 00:21:49 +0330

    Added a greeter page to the editor

--- init.lua
+++ init.lua
@@ -4,26 +4,27 @@ require("core.options")
 --Installed Lazy here
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        error("Error cloning lazy.nvim:\n" .. out)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 --Installed all my plugins here
 require("lazy").setup({
-    require("plugins.neotree"),
-    require("plugins.colortheme"),
-    require("plugins.bufferline"),
-    require("plugins.lualine"),
-    require("plugins.treesitter"),
-    require("plugins.telescope"),
-    require("plugins.LSP"),
-    require("plugins.autocompletion"),
-    require("plugins.autoformatting"),
-    require("plugins.indent-lines"),
-    require("plugins.misc"),
-    require("plugins.comment"),
+	require("plugins.neotree"),
+	require("plugins.colortheme"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.LSP"),
+	require("plugins.autocompletion"),
+	require("plugins.autoformatting"),
+	require("plugins.indent-lines"),
+	require("plugins.misc"),
+	require("plugins.comment"),
+	require("plugins.alpha"),
 })
